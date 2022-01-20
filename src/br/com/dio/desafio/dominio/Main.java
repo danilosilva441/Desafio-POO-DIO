9a8d47247fd655944cd52ba7848af1a3f93890d9
Danilo da Silva Souza <danilosilva441@gmail.com>
2022-01-19 22:05:30 -0300

    Testes

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -2,7 +2,9 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.entities.Bootcamp;
 import br.com.dio.desafio.dominio.entities.Curso;
+import br.com.dio.desafio.dominio.entities.Dev;
 import br.com.dio.desafio.dominio.entities.Mentoria;
 
 public class Main {
@@ -23,15 +25,48 @@ public class Main {
 		mentoria.setDescricao("Aprendendo os conceitos de POO em Java");
 		mentoria.setData(LocalDate.now());
 		
-		
+		/*
 		System.out.println(curso1);
 		System.out.println(curso2);
 		
 		System.out.println("----------------------------------------------------------------------------------------");
 		
 		System.out.println(mentoria);
+		*/
 		
-
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setName("Bootcamp Java Developer");
+		bootcamp.setDescricao("Descriçao Bootcamp Java Develope");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria);
+		
+		
+		
+		Dev devDan = new Dev();
+		devDan.setNome("Danilo");
+		devDan.inscreverBootcamp(bootcamp);
+		System.out.println("Conteudos inscritos Danilo" + devDan.getConteudoInscritos());
+		System.out.println("--------------------------------------------");
+		devDan.progredir();
+		devDan.progredir();
+		devDan.progredir();
+		System.out.println("Conteudos inscritos Danilo" + devDan.getConteudoInscritos());
+		System.out.println("Conteudos concluidos Danilo" + devDan.getConteudosConcluidos());
+		System.out.println("XP" + devDan.calcularTotalXp());
+		
+		
+		System.out.println("--------------------------------------------");
+		
+		Dev devCa = new Dev();
+		devCa.setNome("Camila");
+		devCa.inscreverBootcamp(bootcamp);
+		System.out.println("Conteudos inscritos Camila" + devCa.getConteudoInscritos());
+		System.out.println("--------------------------------------------");
+		devCa.progredir();
+		System.out.println("Conteudos inscritos Camila" + devCa.getConteudoInscritos());		
+		System.out.println("Conteudos concluidos Camila" + devCa.getConteudosConcluidos());
+		System.out.println("XP" + devCa.calcularTotalXp());
 
 	}
 
--- src/br/com/dio/desafio/dominio/entities/Dev.java
+++ src/br/com/dio/desafio/dominio/entities/Dev.java
@@ -2,6 +2,7 @@ package br.com.dio.desafio.dominio.entities;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -14,12 +15,22 @@ public class Dev {
 	}
 	
 	public void inscreverBootcamp(Bootcamp bootcamp) {	
+		this.conteudoInscritos.addAll(bootcamp.getConteudos());
+		bootcamp.getDevsInscritos().add(this);
 	}
 	
 	public void progredir() {
+		Optional<Conteudo> conteudo = this.conteudoInscritos.stream().findFirst();
+		if(conteudo.isPresent()) {
+			this.conteudosConcluidos.add(conteudo.get());
+			this.conteudoInscritos.remove(conteudo.get());
+		} else {
+			System.err.println("Você não está matriculado em nenhum curso.");
+		}
 	}
 	
-	public void calcularTotalXp() {
+	public double calcularTotalXp() {
+		return this.conteudosConcluidos.stream().mapToDouble(conteudo -> conteudo.calcularXp()).sum();
 	}
 
 	public String getNome() {
